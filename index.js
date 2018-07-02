3e7e209fc9ebc97e0cd166b9c6ad77da1c108a1b
Chely Ho <adept_inept@yahoo.com>
2018-07-02 22:34:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const logDriverNames = function(drivers) {
 
 const logDriversByHometown = function(drivers, hometown) {
   drivers.forEach(function(driver) {
-    if driver.hometown === hometown {
+    if (driver.hometown === hometown) {
       console.log(driver.name)
     }
   })
